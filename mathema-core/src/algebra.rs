b9c4aae49aedf662a16135a8095ff45e12c8cb78
Blakely Haughton <blakely.haughton@gmail.com>
2025-09-16 14:40:36 -0400

    implement AlgebraBuilder to replace FunctionBuilder's building code

--- mathema-core/src/algebra.rs
+++ mathema-core/src/algebra.rs
@@ -3,7 +3,14 @@ use std::rc::Rc;
 use crate::{
     function::Function,
     expr::*,
+    context::Context,
 };
+
+#[derive(Debug)]
+pub struct Algebra {
+    pub(crate) params: Vec<String>,
+    pub(crate) tree: AlgebraTree,
+}
 
 pub(crate) type AlgebraNode = Rc<AlgebraTree>;
 
@@ -46,7 +53,7 @@ impl From<f64> for AlgebraTree {
 
 impl From<ParamNode> for AlgebraTree {
     fn from(value: ParamNode) -> Self {
-        ValueNode::Param(value).into()
+        ValueNode::Var(value).into()
     }
 }
 
@@ -68,7 +75,7 @@ impl From<FnCallNode> for AlgebraTree {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct NumNode {
     pub(crate) value: f64
 }
@@ -79,7 +86,7 @@ impl NumNode {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct ParamNode {
     pub(crate) idx: usize
 }
@@ -93,7 +100,7 @@ impl ParamNode {
 #[derive(Debug, PartialEq)]
 pub enum ValueNode {
     Num(NumNode),
-    Param(ParamNode),
+    Var(ParamNode),
 }
 
 impl From<NumNode> for ValueNode {
@@ -104,7 +111,7 @@ impl From<NumNode> for ValueNode {
 
 impl From<ParamNode> for ValueNode {
     fn from(value: ParamNode) -> Self {
-        Self::Param(value)
+        Self::Var(value)
     }
 }
 
@@ -112,7 +119,7 @@ impl ValueNode {
     fn eval(&self, args: &[f64]) -> f64 {
         match self {
             Self::Num(n) => n.eval(),
-            Self::Param(p) => p.eval(args),
+            Self::Var(p) => p.eval(args),
         }
     }
 }
@@ -139,7 +146,7 @@ impl BinaryNode {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum BinaryOperation { Add, Sub, Mul, Div, Exp }
 
 impl From<BinOp> for BinaryOperation {
@@ -200,5 +207,173 @@ impl FnCallNode {
     fn eval(&self, args: &[f64]) -> f64 {
         let args = self.eval_arg_exprs(args);
         self.func.call(&args)
+    }
+}
+
+pub struct AlgebraBuilder<'b> {
+    context: &'b dyn Context,
+    node_queued: Option<AlgebraTree>,
+    params: Vec<String>,
+    errors: Vec<ExprError>,
+}
+
+impl<'b> AlgebraBuilder<'b> {
+    pub fn new(context: &'b dyn Context, params: Vec<String>) -> Self {
+        AlgebraBuilder {
+            context,
+            params,
+            node_queued: None,
+            errors: Vec::new()
+        }
+    }
+
+    pub fn build(mut self, expr: &Expr) -> Result<Algebra, Vec<ExprError>> {
+        if let Some(tree) = self.build_tree(expr) {
+            Ok(Algebra {
+                params: self.params,
+                tree
+            })
+        } else {
+            Err(self.errors)
+        }
+    }
+
+    fn build_tree(&mut self, expr: &Expr) -> Option<AlgebraTree> {
+        self.visit_expr(expr);
+        self.take_node()
+    }
+
+    fn take_node(&mut self) -> Option<AlgebraTree> {
+        self.node_queued.take()
+    }
+
+    fn store_node(&mut self, node: AlgebraTree) {
+        if self.node_queued.is_some() {
+            panic!("AlgebraTreeBuilder already has a node stored")
+        } else {
+            self.node_queued = Some(node)
+        };
+    }
+}
+
+impl<'b> ExprVisit for AlgebraBuilder<'b> {
+    fn visit_value(&mut self, node: &ExprValue) {
+        match node {
+            ExprValue::Literal(l) => self.store_node(
+                NumNode { value: l.num }.into()
+            ),
+            ExprValue::Ident(id) => {
+                // check if ident name has already been seen
+                let new_node = if let Some(found) = self.params.iter().position(|v| *v == *id.repr) {
+                    ParamNode { idx: found }.into()
+                } else {
+                    let value = match self.context.get_variable(&id.repr) {
+                        Some(v) => v,
+                        None => {
+                            self.errors.push(ExprError::UndefinedFunc(id.clone()));
+                            return
+                        }
+                    };
+                    NumNode { value }.into()
+                };
+
+                self.store_node(new_node)
+            },
+        }
+    }
+
+    fn visit_binary(&mut self, node: &ExprBinary) {
+        self.visit_expr(&node.lhs);
+        let left = self.take_node();
+        self.visit_expr(&node.rhs);
+        let right = self.take_node();
+
+        match (left, right) {
+            (Some(l), Some(r)) => self.store_node(
+                BinaryNode {
+                    left: l.into(),
+                    op: node.op.clone().into(),
+                    right: r.into(),
+                }.into()
+            ),
+
+            _ => {}
+        }
+
+    }
+
+    fn visit_unary(&mut self, node: &ExprUnary) {
+        self.visit_expr(&node.expr);
+        let expr = self.take_node();
+
+        if let Some(en) = expr {
+            self.store_node(
+                UnaryNode {
+                    op: node.op.clone().into(),
+                    tree: Box::new(en)
+                }.into()
+            );
+        }
+    }
+
+    fn visit_group(&mut self, node: &ExprGroup) {
+        self.visit_expr(&node.expr);
+    }
+
+    fn visit_fn_call(&mut self, node: &ExprFnCall) {
+        let args: Option<Box<[AlgebraTree]>> = node.inputs.iter()
+            .map(|expr| {
+                self.visit_expr(expr);
+                self.take_node()
+            })
+            .collect();
+
+        let args = match args {
+            Some(a) => a,
+            None => return
+        };
+
+        let func = match self.context.get_function(&node.name.repr) {
+            Some(f) => f,
+            None => {
+                self.errors.push(ExprError::UndefinedFunc(node.name.clone()));
+                return
+            }
+        };
+
+        let new_node = FnCallNode {
+            args,
+            func
+        }.into();
+
+        self.store_node(new_node);
+    }
+}
+
+pub trait AlgebraVisit {
+    fn visit_tree(&mut self, node: &AlgebraTree) {
+        match node {
+            AlgebraTree::Value(n) => self.visit_value(n),
+            AlgebraTree::Binary(n) => self.visit_binary(n),
+            AlgebraTree::Unary(n) => self.visit_unary(n),
+            AlgebraTree::FnCall(n) => self.visit_fn_call(n),
+        }
+    }
+
+    fn visit_value(&mut self, node: &ValueNode) {
+        let _ = node;
+    }
+
+    fn visit_binary(&mut self, node: &BinaryNode) {
+        self.visit_tree(&node.left);
+        self.visit_tree(&node.right);
+    }
+
+    fn visit_unary(&mut self, node: &UnaryNode) {
+        self.visit_tree(&node.tree);
+    }
+
+    fn visit_fn_call(&mut self, node: &FnCallNode) {
+        node.args.iter().for_each(|e| self.visit_tree(e));
     }
 }
--- mathema-core/src/function.rs
+++ mathema-core/src/function.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-
 use crate::{
     algebra::*,
     context::Context,
@@ -18,84 +16,29 @@ impl From<ExprError> for FunctionError {
     }
 }
 
-struct FunctionBuilder<'c> {
+impl FunctionError {
+    pub fn from_vec(value: Vec<ExprError>) -> Vec<Self> {
+        value.into_iter().map(FunctionError::from).collect()
+    }
+}
+
+pub struct FunctionBuilder<'c> {
     fn_name: String,
     params: Vec<String>,
-    context: &'c Context,
+    context: &'c dyn Context,
 }
 
 impl<'c> FunctionBuilder<'c> {
-    fn new(fn_name: String, params: Vec<String>, context: &'c Context) -> Self {
+    pub fn new(fn_name: String, params: Vec<String>, context: &'c dyn Context) -> Self {
         FunctionBuilder { fn_name, params, context }
     }
 
-    fn build_function(self, expr: Expr) -> Result<Function, FunctionError> {
-        let ast = self.convert_algebra_tree(expr)?;
-        let Self { params, .. } = self;
-        Ok(Function::new(self.fn_name, params, ast))
-    }
+    pub fn build_function(self, expr: Expr) -> Result<Function, Vec<FunctionError>> {
+        let alg_builder = AlgebraBuilder::new(self.context, self.params);
+        let algebra: Algebra = alg_builder.build(&expr)
+            .map_err(|e| e.into_iter().map(FunctionError::from).collect::<Vec<_>>())?;
 
-    fn convert_algebra_tree(&self, expr: Expr) -> Result<AlgebraTree, FunctionError> {
-        Ok(match expr {
-            Expr::Value(v) => AlgebraTree::Value(self.convert_value(v)?),
-            Expr::Binary(b) => AlgebraTree::Binary(self.convert_binary(b)?),
-            Expr::Unary(u) => AlgebraTree::Unary(self.convert_unary(u)?),
-            Expr::Group(g) => self.convert_algebra_tree(*g.expr)?,
-            Expr::FnCall(c) => AlgebraTree::FnCall(self.convert_fn_call(c)?),
-        })
-    }
-
-    fn convert_value(&self, value: ExprValue) -> Result<ValueNode, FunctionError> {
-        match value {
-            ExprValue::Literal(l) => Ok(ValueNode::Num(NumNode { value: l.num })),
-            ExprValue::Ident(id) => {
-                let name = &id.repr;
-                // check if it is a function parameter
-                if let Some(idx) = self.params.iter().position(|p| **p == **name) {
-                    return Ok(ValueNode::Param(ParamNode { idx }))
-                } 
-
-                // then check if it is a variable in scope
-                let num = self.context.get_variable(name)
-                    .ok_or(ExprError::UndefinedVar(id))?;
-
-                Ok(ValueNode::Num(NumNode { value: num }))
-            }
-        }
-    }
-
-    fn convert_binary(&self, binary: ExprBinary) -> Result<BinaryNode, FunctionError> {
-        Ok(BinaryNode {
-            left: Rc::new(self.convert_algebra_tree(*binary.lhs)?),
-            op: binary.op.into(),
-            right: Rc::new(self.convert_algebra_tree(*binary.rhs)?)
-        })
-    }
-
-    fn convert_unary(&self, unary: ExprUnary) -> Result<UnaryNode, FunctionError> {
-        Ok(UnaryNode {
-            op: unary.op.into(),
-            tree: Box::new(self.convert_algebra_tree(*unary.expr)?),
-        })
-    }
-
-    fn convert_fn_call(&self, fn_call: ExprFnCall) -> Result<FnCallNode, FunctionError> {
-        // naive recursion check
-        if *fn_call.name.repr == *self.fn_name {
-            return Err(FunctionError::Recursion(fn_call))
-        }
-
-        let name = fn_call.name.repr.as_ref();
-        let func = self.context.get_function(name)
-            .ok_or(ExprError::UndefinedFunc(fn_call.name))?;
-
-        let exprs = fn_call.inputs.values();
-        let args: Result<Box<_>, _> = exprs
-            .into_iter()
-            .map(|e| self.convert_algebra_tree(e))
-            .collect();
-
-        Ok(FnCallNode { args: args?, func })
+        Ok(Function::new(self.fn_name, algebra))
     }
 }
 
@@ -105,8 +48,8 @@ pub struct Function {
 }
 
 impl Function {
-    pub fn new(name: String, params: Vec<String>, ast: AlgebraTree) -> Self {
-        Function { inner: FunctionInner::new_user_defined(name, params, ast) }
+    pub fn new(name: String, ast: Algebra) -> Self {
+        Function { inner: FunctionInner::new_user_defined(name, ast) }
     }
 
     pub(crate) const fn new_builtin(name: &'static str, params: &'static [&'static str], body: fn(&[f64]) -> f64) -> Self {
@@ -126,12 +69,8 @@ impl Function {
     }
 
     pub fn call(&self, args: &[f64]) -> f64 {
+        // TODO: make this a recoverable error
         assert_eq!(args.len(), self.num_params());
-
-        let mut fn_args = Vec::new();
-        for (idx, param) in self.inner.params().into_iter().enumerate() {
-            fn_args.push((param, args[idx]));
-        }
 
         self.inner.call(args)
     }
@@ -141,8 +80,7 @@ impl Function {
 enum FunctionInner {
     UserDefined {
         name: String,
-        params: Vec<String>,
-        body: Box<AlgebraTree>,
+        body: Algebra
     },
     Builtin {
         name: &'static str,
@@ -158,9 +96,8 @@ impl PartialEq for FunctionInner {
 }
 
 impl FunctionInner {
-    fn new_user_defined(name: String, params: Vec<String>, ast: AlgebraTree) -> Self {
-        let body = Box::new(ast);
-        FunctionInner::UserDefined { name, params, body }
+    fn new_user_defined(name: String, body: Algebra) -> Self {
+        FunctionInner::UserDefined { name, body }
     }
 
     const fn new_builtin(name: &'static str, params: &'static [&'static str], body: fn(&[f64]) -> f64) -> Self {
@@ -176,19 +113,20 @@ impl FunctionInner {
 
     fn params(&self) -> Box<[&str]> {
         match self {
-            Self::UserDefined { params, .. } => params.iter().map(|s| &s[..]).collect(),
+            Self::UserDefined { body, .. } => body.params.iter().map(|s| &s[..]).collect(),
             Self::Builtin { params, .. } => params.to_owned().into()
         }
     }
 
     fn num_params(&self) -> usize {
         match self {
-            Self::UserDefined { params, .. } => params.len(),
+            Self::UserDefined { body, .. } => body.params.len(),
             Self::Builtin { params, .. } => params.len(),
         }
     }
 
     fn call(&self, args: &[f64]) -> f64 {
+        // TODO: make this a recoverable error
         assert_eq!(args.len(), self.num_params());
 
         let mut fn_args = Vec::new();
@@ -197,7 +135,7 @@ impl FunctionInner {
         }
 
         match self {
-            FunctionInner::UserDefined { body, .. } => body.eval(args),
+            FunctionInner::UserDefined { body, .. } => todo!(),
             FunctionInner::Builtin { body, .. } => body(args),
         }
     }
@@ -207,8 +145,8 @@ pub fn create_function(
     name: String,
     params: Vec<String>,
     expr: Expr,
-    ctxt: &Context
-) -> Result<Function, FunctionError> {
+    ctxt: &dyn Context
+) -> Result<Function, Vec<FunctionError>> {
     let builder = FunctionBuilder::new(name, params, ctxt);
     builder.build_function(expr)
 }

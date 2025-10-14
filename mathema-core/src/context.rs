56d49c13dbf17aca857e0ce33f3a491ea0642833
Blakely Haughton <blakely.haughton@gmail.com>
2025-10-14 17:17:01 -0400

    refactor cli context code into core context, move tests to parsing.rs

--- mathema-core/src/context.rs
+++ mathema-core/src/context.rs
@@ -1,8 +1,14 @@
 use std::collections::HashMap;
 
 use crate::{
-    algebra::{Algebra, EvalError},
-    intrinsics::{self, ConstFunc}
+    algebra::{Algebra, AlgebraConverter, EvalError},
+    intrinsics::{self, ConstFunc},
+    parsing::{
+        ast::{Stmt, Expr, VarDecl, FnDecl},
+        lexer::{tokenize, LexError},
+        parser::{ParseBuffer, ParseError},
+    },
+    Name
 };
 
 #[derive(Default)]
@@ -60,6 +66,12 @@ impl Context {
     }
 }
 
+pub enum FuncResult {
+    Return(f64),
+    Errors(Vec<EvalError>),
+    NotFound,
+}
+
 pub enum Function<'a> {
     UserDefined(&'a Algebra),
     Builtin(&'a ConstFunc)
@@ -74,8 +86,82 @@ impl Function<'_> {
     }
 }
 
-pub enum FuncResult {
-    Return(f64),
-    Errors(Vec<EvalError>),
-    NotFound,
+pub enum MathemaError {
+    Lexing(LexError),
+    Parsing(ParseError),
+    Eval(EvalError)
+}
+
+pub enum Outcome {
+    Answer(f64),
+    Var(Name, f64),
+    Fn(Name)
+}
+
+pub fn mathema_parse(context: &mut Context, input: &str) -> Result<Outcome, Vec<MathemaError>> {
+    let (buffer, errors) = tokenize(input);
+    if !errors.is_empty() {
+        let errs = errors.into_iter().map(MathemaError::Lexing).collect();
+        return Err(errs)
+    }
+
+    let parsebuffer = ParseBuffer::new(buffer);
+    let stmt = parsebuffer.parse::<Stmt>()
+        .map_err(|e| vec![MathemaError::Parsing(e)])?;
+
+    let outcome = process_statement(context, stmt)
+        .map_err(|e| e.into_iter().map(MathemaError::Eval).collect::<Vec<_>>());
+
+    outcome
+}
+
+fn process_statement(context: &mut Context, stmt: Stmt) -> Result<Outcome, Vec<EvalError>> {
+    match stmt {
+        Stmt::Expr(expr) => process_expr(context, expr),
+        Stmt::VarDecl(var_decl) => process_var_decl(context, var_decl),
+        Stmt::FnDecl(fn_decl) => process_fn_decl(context, fn_decl)
+    }
+}
+
+fn process_expr(context: &mut Context, expr: Expr) -> Result<Outcome, Vec<EvalError>> {
+    let algebra = AlgebraConverter::new(vec![]).build(&expr);
+    let answer = algebra.evaluate(context, &[])?;
+    context.set_var(String::from("ans"), answer);
+    Ok(Outcome::Answer(answer))
+}
+
+fn process_var_decl(context: &mut Context, var_decl: VarDecl) -> Result<Outcome, Vec<EvalError>> {
+    let name = var_decl.var_name.name.clone();
+    let expr = var_decl.expr;
+
+    let algebra = AlgebraConverter::new(vec![]).build(&expr);
+    let answer = algebra.evaluate(context, &[])?;
+
+    if intrinsics::CONSTANTS.contains_key(&*name) {
+        todo!()
+    } else {
+        context.set_var(name.to_string(), answer);
+    }
+
+    Ok(Outcome::Var(name, answer))
+}
+
+pub fn process_fn_decl(context: &mut Context, fn_decl: FnDecl) -> Result<Outcome, Vec<EvalError>> {
+    let name = fn_decl.sig.fn_name.name;
+    let body = fn_decl.body;
+    let params: Vec<_> = fn_decl.sig.inputs
+        .iter()
+        .cloned()
+        .map(|p| p.name.to_string())
+        .collect();
+
+    let algebra = AlgebraConverter::new(params).build(&body);
+
+    if intrinsics::CONST_FNS.contains_key(&*name) {
+        todo!()
+    } else {
+        context.set_func(name.to_string(), algebra);
+    }
+
+    Ok(Outcome::Fn(name))
 }
